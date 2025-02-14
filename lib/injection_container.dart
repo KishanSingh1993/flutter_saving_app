6cbda092df1a05eea3ce9f6a44e6c21e614103ff
KishanSingh1993 <kishang.90@gmail.com>
2025-02-14 18:50:57 +0530

    gradle issues fixed

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -21,39 +21,4 @@ Future<void> init() async {
       sl<AddSavingsUseCase>(), sl<WithdrawSavingsUseCase>(), sl<Box>()));
 }
 
-// import 'package:get_it/get_it.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-// import 'core/database/hive_service.dart';
-// import 'data/repositories/savings_repository.dart';
-// import 'domain/usecases/add_savings_usecase.dart';
-// import 'domain/usecases/withdraw_savings_usecase.dart';
-// import 'presentation/bloc/savings_bloc.dart';
 
-// final sl = GetIt.instance;
-
-// Future<void> init() async {
-//   // Open Hive box inside init
-
-//   sl.registerLazySingleton(() => HiveService());
-//   sl.registerLazySingleton(() => SavingsRepository(sl<HiveService>()));
-//   sl.registerLazySingleton(() => AddSavingsUseCase(sl<SavingsRepository>()));
-//   await Hive.initFlutter();
-//   final savingsBox = await Hive.openBox('savingsBox');
-//   sl.registerLazySingleton(() => savingsBox);
-//   sl.registerFactory(() => SavingsBloc(sl()));
-// }
-
-// // final sl = GetIt.instance;
-
-// // Future<void> init() async {
-// //   sl.registerLazySingleton(() => HiveService());
-// //   sl.registerLazySingleton(() => SavingsRepository(sl()));
-// //   sl.registerLazySingleton(() => AddSavingsUseCase(sl()));
-// //   sl.registerLazySingleton(() => WithdrawSavingsUseCase(sl()));
-// //   //sl.registerFactory(() => SavingsBloc(sl(), sl()));
-
-// //   await Hive.initFlutter();
-// //   final savingsBox = await Hive.openBox('savingsBox');
-// //   sl.registerLazySingleton(() => savingsBox);
-// //   sl.registerFactory(() => SavingsBloc(sl()));
-// // }
